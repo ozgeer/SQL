354a1c91b132ae34e7b6e8cba655fef327e20734
Ozgenur <83001974+ozgeer@users.noreply.github.com>
2022-07-02 13:22:25 -0700

    odev4

---
+++ odev4.sql
@@ -0,0 +1,10 @@
+1  select distinct replacement_cost from film
+
+2  select count (distinct replacement_cost) from film 
+
+3  select count(title) from film where title ilike 't%'and rating='G'
+
+4  select * from country where country like '_____'
+
+5  select * from city where city ilike '%r'
+
