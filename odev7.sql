72bd0bc924ffede9b7b3571ff0f3e0f2e842b266
Ozgenur <83001974+ozgeer@users.noreply.github.com>
2022-07-03 04:10:03 -0700

    odev7

---
+++ odev7.sql
@@ -0,0 +1,15 @@
+1  select rating from film group by rating  
+
+2  select replacement_cost,count(*) from film 
+group by replacement_cost 
+having count(*) >50
+
+3 select store_id,count(*) from customer
+group by store_id
+
+4  select country_id,count(city) from city
+group by country_id 
+order by count(city) desc 
+limit 1
+  
+  
